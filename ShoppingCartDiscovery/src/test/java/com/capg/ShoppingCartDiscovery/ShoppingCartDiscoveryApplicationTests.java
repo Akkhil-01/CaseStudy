d3e4fa85be277629334d03ef9d65d72d6fa2849d
Sugrivu Akkhil <svn.akhil000@gmail.com>
2022-12-02 11:09:49 +0530

    DiscoveryServer

---
+++ ShoppingCartDiscovery/src/test/java/com/capg/ShoppingCartDiscovery/ShoppingCartDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capg.ShoppingCartDiscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingCartDiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
